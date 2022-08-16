59e5f68cea4430489efd9f19e2e850678c9a1bd9
Yusuf simsek <ytoprak396@gmail.com>
2022-08-16 21:54:27 +0200

    frst

--- src/DayGit2.java
+++ src/DayGit2.java
@@ -2,10 +2,7 @@ public class DayGit2 {
     public static void main(String[] args) {
         System.out.println("Git 2. class");
         System.out.println("Git 2. class");
-        System.out.println("Git 2. class");
-        System.out.println("Git 2. class");
-        System.out.println("Git 2. class");
-        System.out.println("Git 2. class");
+
 
 
     }
