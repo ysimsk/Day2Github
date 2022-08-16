a48fc4c505f12ad41efb305faa6a34e18820921c
Yusuf simsek <ytoprak396@gmail.com>
2022-08-17 00:42:29 +0200

    f

--- src/DayGit2.java
+++ src/DayGit2.java
@@ -9,6 +9,7 @@ public class DayGit2 {
         System.out.println("Git 2. class");
         System.out.println("Deneme master");
         System.out.println("Deneme master");
+        System.out.println("Deneme master");
 
 
 
