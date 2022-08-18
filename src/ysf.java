988f0c607824477c432f65db97ec82842b7412b1
Yusuf simsek <ytoprak396@gmail.com>
2022-08-18 23:54:54 +0200

    Update ysf.java

--- src/ysf.java
+++ src/ysf.java
@@ -2,6 +2,8 @@ public class ysf {
     public static void main(String[] args) {
 
         System.out.println("yysf");
+        System.out.println("yysf1");
+        System.out.println("yysf1");
 
     }
 }
