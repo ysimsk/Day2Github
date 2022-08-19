c19aa75fb50dc258238400be56a87e76ab1f8568
Yusuf simsek <ytoprak396@gmail.com>
2022-08-19 23:58:47 +0200

    Update bjk.java

--- src/bjk.java
+++ src/bjk.java
@@ -8,6 +8,9 @@ public class bjk {
         System.out.println("bu sene11 sampiyonuz");
         System.out.println("bu sene12 sampiyonuz");
         System.out.println("bu sene13 sampiyonuz");
+         System.out.println("bu sene14 sampiyonuz");
+        System.out.println("bu sene15 sampiyonuz");
+        System.out.println("bu sene16 sampiyonuz");
 
     }
 }
